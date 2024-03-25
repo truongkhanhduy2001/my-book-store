90f66f91f7caa9f8fcc4d7a8bd185c8bd5351f66
DESKTOP-LGQ0LO9\Duy <truongkhanhduy.data@gmail.com>
2024-03-25 21:21:31 +0700

    Update code 16

--- src/app/components/discount/discount.tsx
+++ src/app/components/discount/discount.tsx
@@ -1,5 +1,5 @@
 "use client";
-import "./dicount.css";
+import "./discount.css";
 import Link from "next/link";
 import Image from "next/image";
 import React, { useRef, useState, useEffect } from "react";
--- src/app/components/seller/seller.tsx
+++ src/app/components/seller/seller.tsx
@@ -39,40 +39,6 @@ export default function Seller() {
     e.preventDefault();
   };
 
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
   return (
     <section className="section-p1 ">
       <div className="section-p1-container">
