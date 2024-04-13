8847b8524c2671c4ac5ab1f736b7354bd70493d3
DESKTOP-LGQ0LO9\Duy <truongkhanhduy.data@gmail.com>
2024-04-13 23:41:20 +0700

    Update code 32

--- src/app/components/arrival/arrival.tsx
+++ src/app/components/arrival/arrival.tsx
@@ -3,9 +3,6 @@ import Link from "next/link";
 import "./arrival.css";
 import Image from "next/image";
 import React, { useRef, useState, useEffect } from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 import { LuEye } from "react-icons/lu";
 import { FiHeart } from "react-icons/fi";
 import { FaShoppingCart } from "react-icons/fa";
@@ -22,13 +19,13 @@ export default function Arrival() {
       title: "Anime",
       price: "100",
       discount: "",
-      time: "old",
+      time: "new",
     },
     {
       title: "Naruto",
       price: "100",
       discount: "",
-      time: "",
+      time: "new",
     },
     {
       title: "Drama",
@@ -43,19 +40,7 @@ export default function Arrival() {
       time: "new",
     },
     {
-      title: "Drama",
-      price: "100",
-      discount: "",
-      time: "new",
-    },
-    {
-      title: "Drama",
-      price: "100",
-      discount: "",
-      time: "old",
-    },
-    {
-      title: "Drama",
+      title: "game",
       price: "100",
       discount: "",
       time: "new",
@@ -102,39 +87,6 @@ export default function Arrival() {
     e.preventDefault();
   };
 
-  // Slider
-  var settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
   return (
     <section className="section-p2" id="newarrival">
       <div className="section-p2-container">
@@ -145,96 +97,91 @@ export default function Arrival() {
       </div>
       <div className="arrivals-container">
         <div className="arrivals">
-          <div className="arrivals-box slider-container">
-            <Slider {...settings}>
-              {data.slice(-4).map((item, index) => {
-                const { discount: discount, price: price, time } = item;
-                const per = (
-                  ((Number(discount) - Number(price)) / Number(price)) *
-                  100
-                ).toFixed(0);
-                return (
-                  <Link key={index} href="#" className="arrivals-card">
-                    {time == "new" && (
-                      <div className="arrivals-label">
-                        <span className="new">NEW</span>
-                      </div>
-                    )}
-                    <div className="arrivals-img">
-                      <Image
-                        src="/images/biasach1.png"
-                        alt="Main Image"
-                        width={100}
-                        height={100}
-                        priority={true}
-                        style={{
-                          maxWidth: "100%",
-                          height: "auto",
-                        }}
-                      ></Image>
+          <div className="arrivals-box">
+            {data.slice(-6).map((item, index) => {
+              const { discount: discount, price: price, time } = item;
+              const per = (
+                ((Number(discount) - Number(price)) / Number(price)) *
+                100
+              ).toFixed(0);
+              return (
+                <Link key={index} href="#" className="arrivals-card">
+                  {time == "new" && (
+                    <div className="arrivals-label">
+                      <span className="new">NEW</span>
                     </div>
-                    <div className="arrivals-tag">
-                      <h2>{item.title}</h2>
-                      <div className="Arrivalwriter">John Deo</div>
-                      <div className="Arrivalcategories">
-                        Thriller, Horror, Romance
-                      </div>
-                      <div className="Arrivalbook-price">
-                        {item.discount != "" && (
-                          <h4
-                            style={{
-                              textDecoration: "none",
-                              color: "hsl(230, 70%, 16%)",
-                              fontWeight: "bold",
-                              marginRight: "8px",
-                            }}
-                          >
-                            ${item.discount}
-                          </h4>
-                        )}
-                        <h3
-                          style={
-                            item.discount != ""
-                              ? {
-                                  textDecoration: "line-through",
-                                  color: "hsl(230, 16%, 45%)",
-                                  fontWeight: "400",
-                                }
-                              : { textDecoration: "none" }
-                          }
+                  )}
+                  <div className="arrivals-img">
+                    <Image
+                      src="/images/biasach1.png"
+                      alt="Main Image"
+                      width={100}
+                      height={100}
+                      priority={true}
+                      style={{
+                        maxWidth: "100%",
+                        height: "auto",
+                      }}
+                    ></Image>
+                  </div>
+                  <div className="arrivals-tag">
+                    <h2>{item.title}</h2>
+                    <div className="Arrivalwriter">John Deo</div>
+                    <div className="Arrivalcategories">
+                      Thriller, Horror, Romance
+                    </div>
+                    <div className="Arrivalbook-price">
+                      {item.discount != "" && (
+                        <h4
+                          style={{
+                            textDecoration: "none",
+                            color: "hsl(230, 70%, 16%)",
+                            fontWeight: "bold",
+                            marginRight: "8px",
+                          }}
                         >
-                          ${item.price}
-                        </h3>
+                          ${item.discount}
+                        </h4>
+                      )}
+                      <h3
+                        style={
+                          item.discount != ""
+                            ? {
+                                textDecoration: "line-through",
+                                color: "hsl(230, 16%, 45%)",
+                                fontWeight: "400",
+                              }
+                            : { textDecoration: "none" }
+                        }
+                      >
+                        ${item.price}
+                      </h3>
 
-                        {item.discount != "" && (
-                          <span className="sale">-{per}%</span>
-                        )}
-                      </div>
-                      <div className="Arrivalcart-btn">
-                        <i>
-                          <FaShoppingCart />
-                        </i>
-                        <p className="add-cart"> Add cart</p>
-                      </div>
-                      <div className="Icon-Container">
-                        <i>
-                          <LuEye />
-                        </i>
-                        <i>
-                          <FaArrowRightArrowLeft />
-                        </i>
-                        <i>
-                          <FiHeart
-                            className="HeartIcon"
-                            onClick={handleHeart}
-                          />
-                        </i>
-                      </div>
+                      {item.discount != "" && (
+                        <span className="sale">-{per}%</span>
+                      )}
                     </div>
-                  </Link>
-                );
-              })}
-            </Slider>
+                    <div className="Arrivalcart-btn">
+                      <i>
+                        <FaShoppingCart />
+                      </i>
+                      <p className="add-cart"> Add cart</p>
+                    </div>
+                    <div className="Icon-Container">
+                      <i>
+                        <LuEye />
+                      </i>
+                      <i>
+                        <FaArrowRightArrowLeft />
+                      </i>
+                      <i>
+                        <FiHeart className="HeartIcon" onClick={handleHeart} />
+                      </i>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
           </div>
         </div>
       </div>
