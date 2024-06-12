1f5c7d5fcf0f0aac64377046a2a12ac55544fc63
DESKTOP-LGQ0LO9\Duy <truongkhanhduy.data@gmail.com>
2024-06-12 21:25:21 +0700

    update

--- src/app/api/admin/loginAdmin/route.ts
+++ src/app/api/admin/loginAdmin/route.ts
@@ -2,17 +2,24 @@ import connectDB from "@/app/lib/connectDB";
 import Admin from "@/app/models/Admin";
 import { NextResponse, NextRequest } from "next/server";
 import { signToken } from "@/app/utils/jwt";
+import bcrypt from "bcryptjs";
 
 export async function POST(req: NextRequest) {
   await connectDB();
 
   try {
+    await new Admin({
+      name: "admin",
+      password: "123456",
+      role: "admin",
+    });
     const { name, password } = await req.json();
+
     const admin = await Admin.findOne({ name });
 
     if (admin) {
-      if (password === admin.password) {
-        // Note: In production, use a secure password comparison like bcrypt
+      const isPasswordMatch = await bcrypt.compare(password, admin.password);
+      if (isPasswordMatch) {
         if (admin.role === "admin") {
           const id = admin._id;
           const token = await signToken({ id, name, role: admin.role });
@@ -31,13 +38,13 @@ export async function POST(req: NextRequest) {
 
       return NextResponse.json({
         success: false,
-        message: "The email or password is incorrect.",
+        message: "The name or password is incorrect.",
       });
     }
 
     return NextResponse.json({
       success: false,
-      message: "The email or password is incorrect.",
+      message: "The name or password is incorrect.",
     });
   } catch (err: any) {
     return NextResponse.json({ success: false, error: err.message });
