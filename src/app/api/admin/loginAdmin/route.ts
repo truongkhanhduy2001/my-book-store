0e1ebadfc29b6196f1166f9829d24bf7fa1aeb03
DESKTOP-LGQ0LO9\Duy <truongkhanhduy.data@gmail.com>
2024-06-12 22:04:27 +0700

    Update

--- src/app/api/admin/loginAdmin/route.ts
+++ src/app/api/admin/loginAdmin/route.ts
@@ -8,43 +8,47 @@ export async function POST(req: NextRequest) {
   await connectDB();
 
   try {
-    await new Admin({
-      name: "admin",
-      password: "123456",
-      role: "admin",
-    }).save();
-    const { name, password } = await req.json();
+    const existingAdmin = await Admin.findOne({ role: "admin" });
 
-    const admin = await Admin.findOne({ name });
+    if (existingAdmin) {
+      const { name, password } = await req.json();
 
-    if (admin) {
-      if (admin.password == password) {
-        if (admin.role === "admin") {
-          const id = admin._id;
-          const token = await signToken({ id, name, role: admin.role });
-          return NextResponse.json({
-            success: true,
-            message: "Admin Login Successful",
-            token: token,
-          });
-        } else {
-          return NextResponse.json({
-            success: false,
-            message: "Access denied. Not an admin.",
-          });
+      const admin = await Admin.findOne({ name });
+
+      if (admin) {
+        if (admin.password == password) {
+          if (admin.role === "admin") {
+            const id = admin._id;
+            const token = await signToken({ id, name, role: admin.role });
+            return NextResponse.json({
+              success: true,
+              message: "Admin Login Successful",
+              token: token,
+            });
+          } else {
+            return NextResponse.json({
+              success: false,
+              message: "Access denied. Not an admin.",
+            });
+          }
         }
-      }
 
+        return NextResponse.json({
+          success: false,
+          message: "The name or password is incorrect.",
+        });
+      }
       return NextResponse.json({
         success: false,
         message: "The name or password is incorrect.",
       });
+    } else {
+      await new Admin({
+        name: "admin",
+        password: "123456", // It's recommended to hash the password before saving
+        role: "admin",
+      }).save();
     }
-
-    return NextResponse.json({
-      success: false,
-      message: "The name or password is incorrect.",
-    });
   } catch (err: any) {
     return NextResponse.json({ success: false, error: err.message });
   }
