f32c05886fa368ae568cad4df9b26392a397f1be
DESKTOP-LGQ0LO9\Duy <truongkhanhduy.data@gmail.com>
2024-06-25 22:25:12 +0700

    Update fix adventure

--- src/app/(navigation-footer)/adventure/page.tsx
+++ src/app/(navigation-footer)/adventure/page.tsx
@@ -5,53 +5,43 @@ import { useEffect, useLayoutEffect, useState } from "react";
 import Paginate from "@/app/components/paginate/paginate";
 import CardBook from "@/app/components/cardBook/cardBook";
 export default function Adventure() {
-  const data = [
-    {
-      title: "Dune",
-      price: "100",
-      discount: "0",
-      type: ["Adventure", "Comedy"],
-      time: "new",
-    },
-    {
-      title: "Anime",
-      price: "100",
-      discount: "0",
-      type: ["Adventure", "Thriller"],
-      time: "old",
-    },
-    {
-      title: "Naruto",
-      price: "100",
-      discount: "0",
-      type: ["Adventure", "Science"],
-      time: "old",
-    },
-    {
-      title: "Drama",
-      price: "100",
-      discount: "0",
-      type: ["Adventure", "Romance"],
-      time: "old",
-    },
-    {
-      title: "Tom",
-      price: "900",
-      discount: "0",
-      type: ["Adventure", "Horror"],
-      time: "old",
-    },
-  ];
+  const [products, setProducts] = useState(null) as any;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [totalProducts, setTotalProducts] = useState(0);
+  const limit = 4; // Limit for admin page
 
-  const [dataLists, setDataList] = useState(data);
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(
+        `/api/admin/productAdmin?page=${currentPage}&limit=${limit}`
+      );
+      const data = await response.json();
+
+      if (data.status === 200) {
+        setProducts(data.products);
+        setTotalPages(data.totalPages);
+        setTotalProducts(data.totalProducts);
+        if (data.products.length == 0) {
+          setCurrentPage(data.totalPages);
+        }
+      }
+    } catch (error) {
+      console.error("Failed to fetch products:", error);
+    }
+  };
+
+  useLayoutEffect(() => {
+    fetchProducts();
+  }, [currentPage]);
 
   // Filter page
-  useLayoutEffect(() => {
-    const filterData = dataLists.filter((item: any) => {
-      return item.type.includes("Adventure");
-    });
-    setDataList(filterData);
-  }, []);
+  // useLayoutEffect(() => {
+  //   const filterData = products.filter((item: any) => {
+  //     return item.type.includes("Adventure");
+  //   });
+  //   setProducts(filterData);
+  // }, []);
 
   return (
     <>
@@ -80,16 +70,29 @@ export default function Adventure() {
         <div className="books-container flex justify-center mt-[var(--margin-top-font)]">
           <div className="books max-w-[var(--width-home)] w-[100%]">
             <div className="books-box grid grid-cols-4 gap-[15px]">
-              {dataLists.map((item, index) => {
-                const { discount: discount, price: price, time } = item;
+              {products?.map((product: any) => {
+                const { discount: discount, price: price, time } = product;
                 const per = (
                   ((Number(discount) - Number(price)) / Number(price)) *
                   100
                 ).toFixed(0);
-                return <CardBook key={index} item={item} per={per} />;
+                return (
+                  <CardBook
+                    key={product._id}
+                    product={product}
+                    per={per}
+                    time={time}
+                  />
+                );
               })}
             </div>
-            <Paginate />
+            {totalProducts > limit && (
+              <Paginate
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={setCurrentPage}
+              />
+            )}
           </div>
         </div>
       </section>
