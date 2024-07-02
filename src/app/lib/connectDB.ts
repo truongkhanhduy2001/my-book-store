0362d3a92de634b2b9bf6f6f25ca764081ec96e3
DESKTOP-LGQ0LO9\Duy <truongkhanhduy.data@gmail.com>
2024-07-02 19:10:45 +0700

    fix

--- src/app/lib/connectDB.ts
+++ src/app/lib/connectDB.ts
@@ -8,8 +8,8 @@ async function connectDB() {
     return;
   }
   const db = await mongoose.connect(
-    // "mongodb+srv://truongkhanhduydata:LljWL6XST4IhhRHB@cluster.qgnzikg.mongodb.net/Bookstore"
-    "mongodb://localhost:27017/Bookstore"
+    "mongodb+srv://truongkhanhduydata:LljWL6XST4IhhRHB@cluster.qgnzikg.mongodb.net/Bookstore"
+    // "mongodb://localhost:27017/Bookstore"
   );
 
   connection.isConnected = db.connections[0].readyState;
