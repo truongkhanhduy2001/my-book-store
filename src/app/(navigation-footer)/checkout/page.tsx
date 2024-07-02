a6164aec1349266bba0a2f64c7d9d42d6ec9bf71
DESKTOP-LGQ0LO9\Duy <truongkhanhduy.data@gmail.com>
2024-07-02 18:48:07 +0700

    Fix Update 6

--- src/app/(navigation-footer)/checkout/page.tsx
+++ src/app/(navigation-footer)/checkout/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import "./checkout.css";
