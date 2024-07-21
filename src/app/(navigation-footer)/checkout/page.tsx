4e6cd53c7c783fe1957eb2268325e12d38ca160b
DESKTOP-LGQ0LO9\Duy <truongkhanhduy.data@gmail.com>
2024-07-21 20:26:02 +0700

    Update 1

--- src/app/(navigation-footer)/checkout/page.tsx
+++ src/app/(navigation-footer)/checkout/page.tsx
@@ -594,7 +594,7 @@ export default function CheckOut() {
                       </div>
                     </Link>
                     <div className="p-4">
-                      <h3 className="text-lg font-semibold mb-2 truncate">
+                      <h3 className="text-lg font-semibold mb-2 text-gray-600">
                         {product.name}
                       </h3>
                       <p className="text-sm text-gray-600 mb-2">
