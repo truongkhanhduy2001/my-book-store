c6183ec9ecc15f1645c315b38c02ffa8ec65ac17
DESKTOP-LGQ0LO9\Duy <truongkhanhduy.data@gmail.com>
2024-08-06 11:20:34 +0700

    Update fix server ver2

--- src/app/api/review/get/route.ts
+++ src/app/api/review/get/route.ts
@@ -5,12 +5,19 @@ import { NextRequest, NextResponse } from "next/server";
 export const revalidate = 0;
 
 export async function GET(req: NextRequest) {
-  await connectDB();
-  const { searchParams } = new URL(req.url);
-  const id = searchParams.get("id");
-  const sort = searchParams.get("sort") || "newest";
-
   try {
+    await connectDB();
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+    const sort = searchParams.get("sort") || "newest";
+
+    if (!id) {
+      return NextResponse.json({
+        status: 400,
+        error: "Product ID is required",
+      });
+    }
+
     let reviews = await Review.find({ productId: id }).populate(
       "userId",
       "name"
