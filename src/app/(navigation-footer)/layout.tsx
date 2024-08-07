017a3f1059a822b52171d94732aaf84d6cd90e32
DESKTOP-LGQ0LO9\Duy <truongkhanhduy.data@gmail.com>
2024-08-07 16:04:00 +0700

    Update finally website book

--- src/app/(navigation-footer)/layout.tsx
+++ src/app/(navigation-footer)/layout.tsx
@@ -1,30 +1,14 @@
 "use client";
-import { useState, useEffect } from "react";
 import { ReactNode } from "react";
 import Navigate from "@/app/components/navigation/navigation";
 import Footer from "@/app/components/footer/footer";
 import Newsletter from "../components/newsletter/newsletter";
-import Loader from "../components/loader/loader";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function LayOut({ children }: LayoutProps) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
-
   return (
     <main>
       {/* Navbar */}
